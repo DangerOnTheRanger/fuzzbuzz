b98dbc0b31dfe8348eb0192cc7c1c1940f1db013
the-zebulan <the-zebulan@users.noreply.github.com>
2016-05-20 22:43:10 -0600

    Removed global variable

--- cycle3.py
+++ cycle3.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 from itertools import cycle, islice
 
-SEQ = ('Buzz', 'Fuzz', 'FuzzBuzz', 'Fuzz', 'Buzz', 'FuzzBuzz')
-
 
 def main(n):
-    for x in islice(cycle(SEQ), n):
+    for x in islice(cycle(('Buzz', 'Fuzz', 'FuzzBuzz',
+                           'Fuzz', 'Buzz', 'FuzzBuzz')), n):
         print(x)
 
 
